f28402f5137e06c1d5e35b51a9051d5bef46033e
Iosvany Dalmau <iosvanyd1@gmail.com>
2019-03-22 02:26:47 -0400

    Development Ready

--- test/test.js
+++ test/test.js
@@ -26,7 +26,7 @@ describe("CRUD user API test", function () {
   it('a new user should be created', function(){
     return new Promise((resolve, reject) => {
       var event={
-        "body":'{"name":"John Poe","gender": "female"}'
+        "body":'{"gender":"female","name":{"title":"ms","first":"anna","last":"lilleng"},"location":{"street":"smestadhagan 4496","city":"hauge","state":"vest-agder","postcode":"6404","coordinates":{"latitude":"51.1252","longitude":"18.3624"},"timezone":{"offset":"+5:30","description":"Bombay, Calcutta, Madras, New Delhi"}},"email":"anna.lilleng@example.com","login":{"uuid":"17cbac4e-6eb8-4a5d-9171-819050135485","username":"bigleopard190","password":"hookers","salt":"4RGPv1Ze","md5":"d8db1a20ad5ed5ffe464c293c1fffb41","sha1":"2802699749457ee833ef8d66259436938e6c3abe","sha256":"14e03cefa2edc913c6a2d5f5092b284f866a521cea5147785d38d69d7aea7aaa"},"dob":{"date":"1974-11-08T11:47:04Z","age":44},"registered":{"date":"2002-05-01T21:49:38Z","age":16},"phone":"20232534","cell":"98679534","id":{"name":"FN","value":"08117437745"},"picture":{"large":"https://randomuser.me/api/portraits/women/34.jpg","medium":"https://randomuser.me/api/portraits/med/women/34.jpg","thumbnail":"https://randomuser.me/api/portraits/thumb/women/34.jpg"},"nat":"NO"}'
       };
       var context={};
       var callback = (ctx, data) => {
@@ -53,9 +53,75 @@ describe("CRUD user API test", function () {
 				else{
 					reject(data);
 				}
-				//done();
 			}
 			funcReadAll(event,context,callback)
 		}).catch();
-	});	
+  });
+  
+  it('should return one users', function(){
+		return new Promise((resolve, reject) => { 
+			var event={
+        "pathParameters": '{"id": "17cbac4e-6eb8-4a5d-9171-819050135485"}'
+      };
+			var context={};
+			var callback = (ctx, data) => {
+			
+				if(data.statusCode == 200){
+					resolve(data);
+				}
+				else{
+					reject(data);
+				}
+			}
+			funcReadOne(event,context,callback)
+		}).catch();
+  });	
+  
+  it('should update one users', function(){
+		return new Promise((resolve, reject) => { 
+			var event={
+        "pathParameters": '{"id": "17cbac4e-6eb8-4a5d-9171-819050135485"}',
+        "body":'{"gender":"male","name":{"title":"ms","first":"anna","last":"lilleng"},"location":{"street":"smestadhagan 4496","city":"hauge","state":"vest-agder","postcode":"6404","coordinates":{"latitude":"51.1252","longitude":"18.3624"},"timezone":{"offset":"+5:30","description":"Bombay, Calcutta, Madras, New Delhi"}},"email":"anna.lilleng@example.com","login":{"uuid":"17cbac4e-6eb8-4a5d-9171-819050135485","username":"bigleopard190","password":"hookers","salt":"4RGPv1Ze","md5":"d8db1a20ad5ed5ffe464c293c1fffb41","sha1":"2802699749457ee833ef8d66259436938e6c3abe","sha256":"14e03cefa2edc913c6a2d5f5092b284f866a521cea5147785d38d69d7aea7aaa"},"dob":{"date":"1974-11-08T11:47:04Z","age":44},"registered":{"date":"2002-05-01T21:49:38Z","age":16},"phone":"20232534","cell":"98679534","id":{"name":"FN","value":"08117437745"},"picture":{"large":"https://randomuser.me/api/portraits/women/34.jpg","medium":"https://randomuser.me/api/portraits/med/women/34.jpg","thumbnail":"https://randomuser.me/api/portraits/thumb/women/34.jpg"},"nat":"NO"}'
+      };
+			var context={};
+			var callback = (ctx, data) => {
+			
+				if(data.statusCode == 200){
+					resolve(data);
+				}
+				else{
+					reject(data);
+				}
+			}
+			funcUpdate(event,context,callback)
+		}).catch();
+  });	
+  
+  it('should delete one users', function(){
+		return new Promise((resolve, reject) => { 
+			var event={
+        "pathParameters": '{"id": "17cbac4e-6eb8-4a5d-9171-819050135485"}'
+      };
+			var context={};
+			var callback = (ctx, data) => {
+			
+				if(data.statusCode == 200){
+					resolve(data);
+				}
+				else{
+					reject(data);
+				}
+			}
+			funcDelete(event,context,callback)
+		}).catch();
+  });	
+
+  it('the notFound function should work', function(done){
+		var event={};
+		var context={};
+		var callback = (ctx, data) => {
+			done();
+		}
+		funcNotFound(event,context,callback)
+	})
 });
